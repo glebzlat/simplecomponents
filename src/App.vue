292c4fb82e6cbd3ca1da46b0520d51cf5cc1bad3
glebzlat <g.zlatanov@dss.kz>
2024-11-20 12:34:05 +0500

    feat: add Switch component

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,47 @@
       :thres-colors='{"70":"yellow","80":"red"}' />
     <input type="text" class="percentage-input" v-model="value" />
   </div>
+  <div class="card">
+    <div class="switch-section switch-color-red">
+      <Switch v-model="switchValue" size="medium" :frozen="false" />
+      <p>{{ switchValue }}</p>
+    </div>
+    <div class="switch-section switch-color-green">
+      <Switch v-model="frozeSwitch" size="medium" :frozen="false" />
+      <Switch v-model="frozenValue" size="medium" :frozen="frozeSwitch" />
+      <p>{{ frozenValue }}</p>
+    </div>
+    <div class="switch-section switch-color-blue">
+      <Switch v-model="masterSwitch" size="medium" :frozen="false" />
+      <Switch v-model="masterSwitch" size="medium" :frozen="true" />
+    </div>
+  </div>
 </div>
 </template>
 
 <script setup>
-  import CircularIndicator from './components/CircularIndicator.vue';
   import { ref } from 'vue';
+
+  import CircularIndicator from './components/CircularIndicator.vue';
+  import Switch from './components/Switch.vue';
 
   const label = 'CPU';
   const value = ref('100');
 
+  const switchValue = ref(false);
+  const frozeSwitch = ref(false);
+  const frozenValue = ref(false);
+  const masterSwitch = ref(false);
+
 </script>
 
 <style scoped>
+.page {
+  display: flex;
+  gap: 15px;
+  flex-wrap: wrap;
+}
+
 .card {
   display: flex;
   flex-direction: column;
@@ -28,5 +56,33 @@
   border: 1px solid grey;
   border-radius: 5px;
   width: fit-content;
+}
+
+.switch-section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  width: 200px;
+  border: 1px solid grey;
+  border-radius: 5px;
+}
+
+.switch-color-red {
+  --switch-active-color: #cc2e2e;
+  --switch-inactive-color: #ccc;
+  --switch-slider-color: #fff;
+}
+
+.switch-color-green {
+  --switch-active-color: #2ecc71;
+  --switch-inactive-color: #ccc;
+  --switch-slider-color: #fff;
+}
+
+.switch-color-blue {
+  --switch-active-color: #397dff;
+  --switch-inactive-color: #ccc;
+  --switch-slider-color: #fff;
 }
 </style>
---
+++ src/components/Switch.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="switch">
+    <div class="slider" :class="{ checked: modelValue, frozen: frozen }"
+        @click="toggle"></div>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    size: {
+      type: [String, Number],
+      default: 1
+    },
+    modelValue: {
+      required: true,
+      type: Boolean
+    },
+    frozen: {
+      type: Boolean,
+      default: false
+    }
+  });
+
+  const emit = defineEmits(['update:modelValue']);
+
+  function toggle() {
+    if (!props.frozen)
+      emit('update:modelValue', !props.modelValue);
+  }
+
+  let ratio;
+  if (typeof props.size === 'string') {
+    switch (props.size.toLowerCase()) {
+    case 'small':
+      ratio = 0.5;
+      break;
+    case 'medium':
+      ratio = 1;
+      break;
+    case 'big':
+      ratio = 1.5;
+      break;
+    default:
+      console.warn(`unknown size value: ${props.size}`)
+      ratio = 1;
+      break;
+    }
+  } else {
+    ratio = props.size;
+  }
+
+</script>
+
+<style scoped>
+.switch {
+  position: relative;
+  width: calc(46px * v-bind('ratio'));
+  height: calc(24px * v-bind('ratio'));
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transition: 0.4s;
+  background-color: var(--switch-inactive-color);
+  border-radius: calc(24px * v-bind('ratio'));
+}
+
+.slider.frozen {
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+
+.slider.checked {
+  background-color: var(--switch-active-color);
+}
+
+.slider::before {
+  position: absolute;
+  content: "";
+  width: calc(16px * v-bind('ratio'));
+  aspect-ratio: 1 / 1;
+  left: calc(4px * v-bind('ratio'));
+  bottom: calc(4px * v-bind('ratio'));
+  background-color: var(--switch-slider-color);
+  transition: 0.4s;
+  border-radius: 50%;
+}
+
+.switch:hover .slider {
+  box-shadow: 0 0 calc(6px * v-bind('ratio')) var(--switch-active-color);
+}
+
+.switch:hover .slider.frozen {
+  box-shadow: none;
+}
+
+.checked::before {
+  transform: translateX(calc(22px * v-bind('ratio')));
+}
+
+</style>
