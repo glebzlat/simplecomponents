e5ead2b2b755c35e64f56e151617d3b0338f2245
glebzlat <g.zlatanov@dss.kz>
2024-11-25 10:03:00 +0500

    build: move index.js to the root as main is not specified in package.json

---
+++ index.js
@@ -0,0 +1,17 @@
+import CircularIndicator from './src/components/CircularIndicator.vue';
+import Switch from './src/components/Switch.vue';
+import Checkbox from './src/components/Checkbox.vue';
+import Tooltip from './src/components/Tooltip.vue';
+import Dropdown from './src/components/Dropdown.vue';
+import Button from './src/components/Button.vue';
+import Menu from './src/components/Menu.vue';
+
+export {
+  CircularIndicator,
+  Switch,
+  Checkbox,
+  Tooltip,
+  Dropdown,
+  Button,
+  Menu
+};
