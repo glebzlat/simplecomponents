c64b8c7614f763a312969037c0ec6968e1b9faf5
glebzlat <g.zlatanov@dss.kz>
2024-11-25 09:42:03 +0500

    build: add index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import CircularIndicator from './components/CircularIndicator.vue';
+import Switch from './components/Switch.vue';
+import Checkbox from './components/Checkbox.vue';
+import Tooltip from './components/Tooltip.vue';
+import Dropdown from './components/Dropdown.vue';
+import Button from './components/Button.vue';
+import Menu from './components/Menu.vue';
+
+export {
+  CircularIndicator,
+  Switch,
+  Checkbox,
+  Tooltip,
+  Dropdown,
+  Button,
+  Menu
+};
