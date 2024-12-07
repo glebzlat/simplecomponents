15885473538399a204fbd3e2882eb11f0179554d
glebzlat <mrkotofey@inbox.ru>
2024-12-07 20:37:15 +0500

    feat: slightly change an appearance of a checkmark

--- src/components/SimpleCheckbox.vue
+++ src/components/SimpleCheckbox.vue
@@ -2,15 +2,15 @@
   <label class="checkbox-container">
     <input
       type="checkbox"
-      class="checkbox"
+      class="checkbox-input"
       @click="toggle"
       @keypress.enter="toggle"
       :tabindex="props.frozen ? -1 : 0"
     >
     <div
-      class="checkmark"
+      class="checkbox"
       :class="{ checked: modelValue, frozen: frozen }"
-    />
+    ><div class="checkmark"></div></div>
     <slot />
   </label>
 </template>
@@ -81,7 +81,7 @@
   cursor: pointer;
 }
 
-.checkbox {
+.checkbox-input {
   position: absolute;
   top: 0;
   opacity: 0;
@@ -90,7 +90,7 @@
   cursor: pointer;
 }
 
-.checkmark {
+.checkbox {
   position: relative;
   width: calc(1.25em * v-bind('ratio'));
   aspect-ratio: 1 / 1;
@@ -100,44 +100,43 @@
   font-size: calc(1.25em * v-bind('ratio'));
 }
 
-.checkmark.checked {
+.checkbox.checked {
   background-color: var(---checkbox-active-color);
 }
 
-.checkmark.frozen {
+.checkbox.frozen {
   opacity: 0.7;
   cursor: not-allowed;
 }
 
-.checkbox-container:focus-within .checkmark,
-.checkmark:hover {
+.checkbox-container:focus-within .checkbox,
+.checkbox:hover {
   box-shadow: 0 0 0.062em calc(0.06em * v-bind('ratio'))
     var(---checkbox-focus-color);
 }
 
-.checkmark.frozen {
+.checkbox.frozen {
   box-shadow: none;
 }
 
-.checkbox-container:focus-within .checkmark.frozen {
+.checkbox-container:focus-within .checkbox.frozen {
   box-shadow: none;
 }
 
-.checkmark::after {
+.checkmark {
   position: absolute;
-  content: "";
   opacity: 0;
   transition: 0.1s;
-  top: 0.1em;
-  left: 0.3em;
-  height: 0.7em;
-  width: 0.4em;
+  top: 0.05em;
+  left: 0.35em;
+  bottom: 0.2em;
+  right: 0.3em;
   border: solid #fff;
   border-width: 0 0.15em 0.15em 0;
   transform: rotate(45deg);
 }
 
-.checkmark.checked::after {
+.checkbox.checked .checkmark {
   opacity: 1;
 }
 </style>
