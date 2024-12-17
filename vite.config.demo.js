b7fb67d435cc753aa92b560b321157686d247cd8
Gleb Zlatanov <g.zlatanov@dss.kz>
2024-12-17 09:34:16 +0500

    build: move demo config to the root
    To eliminate the difference between dev and build import paths

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -1,21 +1,17 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from "vite";
-import { resolve } from 'node:path';
-
-console.log(__dirname)
 
 export default defineConfig({
-  root: __dirname,
   build: {
     rollupOptions: {
-      input: resolve(__dirname, './index.html')
+      input: './demo/index.html'
     },
-    outDir: '../dist.demo',
+    outDir: './dist.demo',
     emptyOutDir: true,
     assetsDir: 'assets'
   },
   server: {
-    open: './index.html'
+    open: './demo/index.html'
   },
   plugins: [vue()]
 });
